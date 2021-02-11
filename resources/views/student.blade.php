4550a00bf4daa8f26c3c18c649aa20292f269f7d
Nirjhor95 <nirjhor@sagbrain.com>
2021-02-11 21:29:13 +0600

    blade and database created

---
+++ resources/views/student.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Information</title>
+</head>
+<body>
+    <h1>Studnent Information Table</h1>
+
+    <table>
+    <tr>
+        <th>Student ID</th>
+        <th>14201</th>
+    </tr>
+    <tr>
+        <td>Name</td>
+        <td>Bunny</td>
+    </tr>
+    <tr>
+        <td>Class</td>
+        <td>2nd</td>
+    </tr>
+    <tr>
+        <td>Course</td>
+        <td>Shooting</td>
+    </tr>
+    </table>
+
+</body>
+</html>
